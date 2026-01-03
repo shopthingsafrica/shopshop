81860dc7507e3f98c4d17ff8b7d106bda5ff5bf7
stephenmayowa112 <stephenmayowa112@gmail.com>
2026-01-03 13:41:52 +0100

    Remove duplicate export in messaging index

--- src/components/messaging/index.ts
+++ src/components/messaging/index.ts
@@ -2,4 +2,3 @@ export { ConversationList } from './ConversationList';
 export { MessageThread } from './MessageThread';
 export { MessagingContainer } from './MessagingContainer';
 export { StartConversationButton } from './StartConversationButton';
-export { StartConversationButton } from './StartConversationButton';
